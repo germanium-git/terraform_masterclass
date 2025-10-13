6c2f8126c03c7d79f962e72b0520bc715c480558
Petr Nemec <germanium-git@gmx.com>
2025-10-13 15:54:58 +0200

    merge ...

--- 01-expand_function_arguments/merge.tf
+++ 01-expand_function_arguments/merge.tf
@@ -12,3 +12,22 @@ output "merged" {
   value       = local.merged
   description = "Merged map from multiple maps"
 }
+
+locals {
+  databases = [
+    {
+      "prod_db" : { name = "prod-db", tier = "db.t3.medium" }
+      "prod_app" : { name = "prod-app", tier = "app.t3.medium" }
+    },
+    {
+      "stage_db" : { name = "stage-db", tier = "db.t3.small" }
+      "stage_app" : { name = "stage-app", tier = "app.t3.small" }
+    },
+    {
+      "dev_db" : { name = "dev-db", tier = "db.t3.micro" }
+      "dev_app" : { name = "dev-app", tier = "app.t3.micro" }
+    }
+  ]
+
+  db_merged_expanded = merge(local.databases...)
+}
